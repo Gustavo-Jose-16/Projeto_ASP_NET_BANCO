ae9f2fb821a65fa5b405c8b0c01f2c0e55681eee
GUSTAVO JOSE ANDRADE DA SILVA <00001118636387sp@aluno.educacao.sp.gov.br>
2026-04-28 15:26:58 -0300

    adicionando

---
+++ Projeto_BancoDados/Repository/UsuarioRepository.cs
@@ -0,0 +1,34 @@
+﻿using Projeto_BancoDados.Models;
+using Projeto_BancoDados.Repository.Contract;
+
+namespace Projeto_BancoDados.Repository
+{
+    public class UsuarioRepository : IUsuarioRepository
+    {
+        private readonly string _conexaoMySQL;
+
+        public UsuarioRepository(IConfiguration conf)
+        {
+            _conexaoMySQL = conf.GetConnectionString("ConexaoMySQL");  
+        }
+        public void AtualizarUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CadastrarUsuario(Usuario usuario)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Usuario> ObterTodosUsuarios()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Usuario ObterUsuario(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
